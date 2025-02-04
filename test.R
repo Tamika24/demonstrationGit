0252949e53dfeab6b134eba471d4f37f75b9b955
Tamika Surajpal <srjtam001@myuct.ac.za>
2025-02-04 12:13:31 +0200

    new line added

--- test.R
+++ test.R
@@ -7,6 +7,8 @@ gert::git_config_global_set("user.name", "Tamika Surajpal")
 gert::git_config_global_set("user.email", "srjtam001@myuct.ac.za")
 
 usethis::create_github_token()
+
 x<-1
 y<-1
 
+gitcreds::gitcreds_set()
