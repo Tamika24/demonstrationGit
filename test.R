2f52da368e26ee8ad61415371c63e08daa29cf8d
Tamika Surajpal <srjtam001@myuct.ac.za>
2025-02-04 11:43:33 +0200

    add demonstartion file

---
+++ test.R
@@ -0,0 +1,9 @@
+if (!requireNamespace("usethis", quietly = TRUE)) {
+  install.packages("usethis")
+}
+usethis::use_git()
+
+gert::git_config_global_set("user.name", "Tamika Surajpal")
+gert::git_config_global_set("user.email", "srjtam001@myuct.ac.za")
+
+usethis::create_github_token()
